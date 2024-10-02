bfc64ce91f0346f72caa196c3ee545637cd7b8d2
dqmis <dom.seputis@gmail.com>
2024-10-02 19:33:14 +0200

    Adding new stuff

--- src/utils/datasets.py
+++ src/utils/datasets.py
@@ -1,4 +1,4 @@
-from datasets import load_dataset
+from datasets import concatenate_datasets, load_dataset
 from transformers import AutoTokenizer
 
 from src.utils.preprocessors import (
@@ -8,6 +8,9 @@ from src.utils.preprocessors import (
 
 DATASETS = {
     "flores": lambda source_lang, target_lang, tokenizer: load_flores_dataset(
+        source_lang, target_lang, tokenizer
+    ),
+    "ntrex": lambda source_lang, target_lang, tokenizer: load_ntrex_dataset(
         source_lang, target_lang, tokenizer
     ),
     "helsinki": lambda dataset_name, source_lang, target_lang, prefix, tokenizer: load_helsinki_dataset(  # noqa
@@ -21,6 +24,27 @@ def load_flores_dataset(source_lang: str, target_lang: str, tokenizer: AutoToken
         "facebook/flores",
         f"{source_lang}_Latn-{target_lang}_Latn",
     )
+    processed_dataset: dict = dataset.map(
+        preprocess_flores_function,
+        batched=True,
+        fn_kwargs={
+            "tokenizer": tokenizer,
+            "source_lang": "eng",
+            "target_lang": "lit",
+        },
+    )
+    return processed_dataset
+
+
+def load_ntrex_dataset(source_lang: str, target_lang: str, tokenizer: AutoTokenizer) -> dict:
+    source_dataset = load_dataset("davidstap/NTREX", f"{source_lang}_Latn")["test"]
+    target_dataset = load_dataset("davidstap/NTREX", f"{target_lang}_Latn")["test"]
+
+    source_dataset = source_dataset.rename_column("text", f"sentence_{source_lang}_Latn")
+    target_dataset = target_dataset.rename_column("text", f"sentence_{target_lang}_Latn")
+
+    dataset = concatenate_datasets([source_dataset, target_dataset], axis=1)
+
     processed_dataset: dict = dataset.map(
         preprocess_flores_function,
         batched=True,
